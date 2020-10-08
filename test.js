9d1f301b9c8d33f619b0db04e83009d8303495c9
naganthanganeshbabu <naganathanganeshbabu@syncfusion.com>
2020-10-08 20:10:42 +0530

    Files are add for hosting.

---
+++ test.js
@@ -0,0 +1,8 @@
+﻿window.getDiagramSize = function () {
+    var element = document.getElementById("canvas_layer_div");
+    var bounds = element.getBoundingClientRect();
+    var size = {};
+    size["X"] = bounds.left;
+    size["Y"] = bounds.top;
+    return size;
+}
